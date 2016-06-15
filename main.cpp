fe3ba4573bd0c53e2f7dc1e7abe2093471d00a91
Arthur Goodman <arthur.goodman3@gmail.com>
2016-06-15 23:02:16 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -5,8 +5,6 @@
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
-    qDebug() << Constraint(QPointF(2, 2), QPointF(5, 5)).intersect(Constraint(QPointF(1, 4), QPointF(1, 1)));
-
     Widget w;
     w.show();
 
